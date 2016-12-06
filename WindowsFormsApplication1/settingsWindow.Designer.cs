77fd8609b90b7952992543bd803734ae7c48c00e
CT12543 <tycamzach@gmail.com>
2016-12-06 14:21:05 -0600

    Added username and password box in settings

--- WindowsFormsApplication1/settingsWindow.Designer.cs
+++ WindowsFormsApplication1/settingsWindow.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.instructionTxt = new System.Windows.Forms.Label();
+            this.textBoxUsermane = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPsswd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(337, 376);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(337, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +49,99 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // instructionTxt
+            // 
+            this.instructionTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.instructionTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.instructionTxt.Location = new System.Drawing.Point(3, 9);
+            this.instructionTxt.Name = "instructionTxt";
+            this.instructionTxt.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.instructionTxt.Size = new System.Drawing.Size(418, 21);
+            this.instructionTxt.TabIndex = 61;
+            this.instructionTxt.Text = "These settings should be set up properly and not need modification.\r\n";
+            // 
+            // textBoxUsermane
+            // 
+            this.textBoxUsermane.Location = new System.Drawing.Point(74, 55);
+            this.textBoxUsermane.Name = "textBoxUsermane";
+            this.textBoxUsermane.Size = new System.Drawing.Size(338, 20);
+            this.textBoxUsermane.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "FTP Server Login Information";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 2);
+            this.label2.TabIndex = 64;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 65;
+            this.label3.Text = "Username";
+            // 
+            // textBoxPsswd
+            // 
+            this.textBoxPsswd.Location = new System.Drawing.Point(74, 81);
+            this.textBoxPsswd.Name = "textBoxPsswd";
+            this.textBoxPsswd.Size = new System.Drawing.Size(338, 20);
+            this.textBoxPsswd.TabIndex = 66;
+            this.textBoxPsswd.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 67;
+            this.label4.Text = "Password";
+            // 
             // settingsWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 411);
+            this.ClientSize = new System.Drawing.Size(424, 135);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPsswd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUsermane);
+            this.Controls.Add(this.instructionTxt);
             this.Controls.Add(this.button1);
             this.Name = "settingsWindow";
             this.Text = "Settings";
             this.Load += new System.EventHandler(this.settingsWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label instructionTxt;
+        private System.Windows.Forms.TextBox textBoxUsermane;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPsswd;
+        private System.Windows.Forms.Label label4;
     }
 }
